c93040aed61b2a26278f49dfa5fd90777a6be856
Ryo Matsumoto <e165738@ie.u-ryukyu.ac.jp>
2016-12-03 22:30:28 +0900

    finished Step2.

--- src/Reigai.java
+++ src/Reigai.java
@@ -1,10 +1,18 @@
-/**
- * Created by M.R on 2016/12/03.
- */
 public class Reigai {
     public static void main(String[] args){
         String str = null;
 
-        System.out.println(str.length());
+        try {
+            System.out.println(str.length());
+        } catch (NullPointerException e){
+            System.out.println("\nError");
+            System.out.println("〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜");
+            System.out.println("\nぬるぽ(NullPointerException)が発生しました！");
+            System.out.println("\ne.toString()    → " + e.toString());
+            System.out.println("\ne.getMessage()  → " + e.getMessage());
+            System.out.print("\ne.printStackTrace()\n→ ");
+            e.printStackTrace();
+            System.out.println("\n〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜\n");
+        }
     }
 }
