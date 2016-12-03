d069bbd45fd6f285bdee32006cf91878a742c990
Ryo Matsumoto <e165738@ie.u-ryukyu.ac.jp>
2016-12-03 20:43:25 +0900

    finished Step1.

---
+++ src/Reigai.java
@@ -0,0 +1,10 @@
+/**
+ * Created by M.R on 2016/12/03.
+ */
+public class Reigai {
+    public static void main(String[] args){
+        String str = null;
+
+        System.out.println(str.length());
+    }
+}
